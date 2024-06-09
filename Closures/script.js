b8c93ac5da5d92d9b603a7b9774a7c5613cf12d5
atharvakasodekar04 <97936616+atharvakasodekar04@users.noreply.github.com>
2024-06-10 01:42:47 +0530

    Initial closures

--- Closures/script.js
+++ Closures/script.js
@@ -1,2 +1,30 @@
-let a = 10;
-console.log(a);
+function z(){
+    var b = 100;
+    function x(){
+        var a = 120;
+        function y(){
+            console.log(a, b);
+        }
+        a = 100;
+        y();
+    }
+    x();
+}
+z();
+
+//output  : 100,100
+
+
+// function x(){
+//     var a = 4;
+//     function  y(){
+//         console.log(a);
+
+//     }
+//     return y;
+
+// }
+// var z = x();
+// console.log(z); //returns a function
+
+// z(); //returns a value inside a function
